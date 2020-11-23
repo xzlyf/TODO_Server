49c4e8017c6ad737444de5e78e592388769cb14c
xzlyf <czr2001@outlook.com>
2020-11-24 00:19:55 +0800

    token1

--- src/main/java/com/xz/app/todolist/pojo/User.java
+++ src/main/java/com/xz/app/todolist/pojo/User.java
@@ -48,16 +48,18 @@ public class User implements Serializable {
     @Column(name = "user_phone", length = 16)
     private String userPhone;
 
-
+    @JsonIgnore
+    @Column(name = "token", length = 32)
+    private String token;
 
     @CreatedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     @LastModifiedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
 }
