afa1eee0da8dea75cfe7960652267005eec89d93
xzlyf <czr2001@outlook.com>
2020-12-02 22:41:24 +0800

    转为https项目

---
+++ src/main/java/com/xz/app/todolist/config/Http2Https.java
@@ -0,0 +1,56 @@
+package com.xz.app.todolist.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * http重定向https
+ */
+@Configuration
+public class Http2Https {
+
+    // 在某配置类中添加如下内容
+    // 监听的http请求的端口,需要在application配置中添加http.port=端口号  如80
+    @Value("${server.port}")
+    private int sslPort;//https的端口
+    //正常启用的https端口 如443
+    @Value("${server.http-port}")
+    private int httpPort;//http的端口
+
+
+    // springboot2 写法
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(sslPort);
+        return connector;
+    }
+}
